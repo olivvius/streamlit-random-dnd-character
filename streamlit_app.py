8484c3704c0f4f37b180d259563de6be674fe2ca
olivier <olivier.lebot22@gmail.com>
2023-08-14 17:28:14 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,22 +3,28 @@ import pandas as pd
 import random
 
 # Load CSV files
-#traits_data = pd.read_csv('personality_traits.csv')
+races_data = pd.read_csv('races.csv')
 classes_data = pd.read_csv('classes.csv')
 alignments_data = pd.read_csv('alignments.csv')
 backgrounds_data = pd.read_csv('backgrounds.csv')
+appearances_data = pd.read_csv('appearances.csv')
+
 
 # Create Streamlit app
 st.title("Random Character Generator")
 
 # Randomly select character attributes
-#random_trait = random.choice(traits_data['Trait'])
+random_race = random.choice(races_data['Race'])
 random_class = random.choice(classes_data['Class'])
 random_alignment = random.choice(alignments_data['Alignment'])
 random_background = random.choice(backgrounds_data['Background'])
+random_appearance = random.choice(appearances_data['Appearance'])
+
 
 # Display random character attributes
-#st.write(f"**Personality Trait:** {random_trait}")
+st.write(f"**Race:** {random_race}")
 st.write(f"**Class:** {random_class}")
 st.write(f"**Alignment:** {random_alignment}")
 st.write(f"**Background:** {random_background}")
+st.write(f"**Appearance:** {random_appearance}")
+
